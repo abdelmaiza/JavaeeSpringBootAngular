48e5a964d3018be70719b1fe3f352bf1b36911d1
amaiza <abdeldjallil.maiza@gmail.com>
2019-03-27 09:32:32 +0100

    add security level

--- bike-ui/src/app/app-routing.module.ts
+++ bike-ui/src/app/app-routing.module.ts
@@ -3,8 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {AdminComponent} from "./components/admin/admin.component";
 import {HomeComponent} from "./components/home/home.component";
 import {ViewRegistrationComponent} from "./components/view-registration/view-registration.component";
+import {CallbackComponent} from "./components/callback/callback.component";
 
 const routes: Routes = [
+  {
+    phat:'rollback',
+    component:CallbackComponent
+  },
   {
     path: '',
     component: HomeComponent
